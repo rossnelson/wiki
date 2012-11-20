d577719dc23e16cd784098969f0cc17c8eb3c714
Josh <joshua.mejia@gmail.com>
2012-11-20 15:53:22 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kozak::Application.config.secret_token = '361f11e49041af2de813b6f18308027c40243c7e02ff9cd43466609ccbc8748385c450ed46df4772ef0cd227d0d6e57108c53459e305f1e900180ef79ec8913b'
