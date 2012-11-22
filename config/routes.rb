289229c606d278e946fdaa651bf7e8c7a054fdf4
Josh Mejia <joshua.mejia@gmail.com>
2012-11-22 14:07:41 -0600

    Added sessions controller and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Kozak::Application.routes.draw do
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
   resources :users
+  resources :sessions
+  root :to => "users#index"
 end
